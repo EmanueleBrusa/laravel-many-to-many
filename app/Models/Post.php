28c7ca8f43148cdd8d080ae73adf24fb8d6c9d1e
Emanuele Brusa <128056225+EmanueleBrusa@users.noreply.github.com>
2023-08-31 16:25:03 +0200

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'content', 'cover_image', 'slug', 'type_id'];
+
+    public static function generateslug($title)
+    {
+        return Str::slug($title, '-');
+    }
+    
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
